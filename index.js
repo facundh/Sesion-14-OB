0e8086e01a7d29221e23db5f873895d00785ac55
Facundo Herrera <facundhfed@gmail.com>
2022-07-15 07:33:20 +0200

    Sesion 14 finalizada

---
+++ index.js
@@ -0,0 +1,9 @@
+const boton = document.querySelector('#button');
+console.log(boton);
+boton.addEventListener('click', () => {
+    console.log('click en el botón');
+})
+boton.addEventListener('click', () => {
+    console.log('Hola, estoy utilizando jQuery"');
+})
+
